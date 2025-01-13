af816010c1a53b3641b93843fe364b3ece112741
ruehlef <f.ruehle@northeastern.edu>
2025-01-13 15:41:57 -0500

    cythonized for python3.11

--- setup_cython.py
+++ setup_cython.py
@@ -1,6 +1,14 @@
 import setuptools
 from Cython.Build import cythonize
 
+# setuptools.setup(
+#     ext_modules=cythonize("src/rw.pyx")
+# )
+
+# need to add language level for python 3.11 or higher
 setuptools.setup(
-    ext_modules=cythonize("src/rw.pyx")
+    ext_modules=cythonize(
+        "src/rw.pyx",  # Path to your Cython file
+        language_level="3",  # Set language level explicitly to match Python 3.x
+    )
 )
